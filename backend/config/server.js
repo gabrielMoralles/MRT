80d7fc78664a83ab8576025b9c8fe8b204961a3e
gabrielMoralles <gabriel.barbosa@fcamara.com.br>
2020-04-02 21:58:37 -0300

    teste

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,24 +1,19 @@
 const express = require('express');
-const consign = require('consign')
+const consign = require('consign');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'http://localhost:4200',
-    optionsSuccessStatus: 200
-}
+	origin: 'https://gabrielmoralles.github.io/MRT/',
+	optionsSuccessStatus: 200
+};
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-consign()
-	.include('app/routes')
-	.then('config/dbConnection.js')
-	.then('app/models')
-	.then('app/controllers')
-    .into(app);
-    
+consign().include('app/routes').then('config/dbConnection.js').then('app/models').then('app/controllers').into(app);
+
 module.exports = app;
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -10,18 +10,18 @@ headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 })
 export class LoginService {
 
-  
+
   private productNameURL = 'http://localhost:9095/login'; //URL to web api
 
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
 
-  getLogin(): Observable <Usuario[]> {
+  getLogin(): Observable<Usuario[]> {
     return this.http.get<any[]>(this.productNameURL)
   }
   postLogin(login: Usuario) {
-    let url = 'http://localhost:9095/login'
-    
-    return this.http.post<Usuario>(url,login)
+    let url = 'http://ec2-3-16-206-59.us-east-2.compute.amazonaws.com:9095/login'
+
+    return this.http.post<Usuario>(url, login)
   }
 }
