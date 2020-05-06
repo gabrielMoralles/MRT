02b5fa8b333e30071343542de84f5849352e061c
gabrielMoralles <gabriel.barbosa@fcamara.com.br>
2020-05-06 20:16:36 -0300

    url

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Usuario } from '../login/model/user_model';
+import { ambiente } from '../env'
 
 headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 
@@ -9,28 +10,32 @@ headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   providedIn: 'root'
 })
 export class LoginService {
+  private loginURL = ''
 
 
-  private productNameURL = 'http://54.233.179.45:9095/login'; //URL to web api
+  constructor(private http: HttpClient,
 
-  constructor(private http: HttpClient) { }
+  ) {
+    ambiente == 'dev' ? this.loginURL = 'http://localhost:9095' : this.loginURL = 'http://ec2-54-233-179-45.sa-east-1.compute.amazonaws.com:9095'
+  }
 
 
   getLogin(): Observable<Usuario[]> {
-    return this.http.get<any[]>(this.productNameURL)
+    let url = `${this.loginURL}/login`
+    return this.http.get<any[]>(url)
   }
   postLogin(login: Usuario) {
-    let url = 'http://ec2-54-233-179-45.sa-east-1.compute.amazonaws.com:9095/'
+    let url = `${this.loginURL}/login`
 
     return this.http.post<Usuario>(url, login)
   }
   forgotPass(email: string, user: Usuario) {
-    let url = 'http://localhost:9095/forgot'
+    let url = `${this.loginURL}/forgot`
     let data = { email, user }
     return this.http.post<Usuario[]>(url, data)
   }
   postPass(pass: string, email: string, user: Usuario) {
-    let url = 'http://localhost:9095/changePass'
+    let url = `${this.loginURL}/changePass`
     let data = { email, user, pass }
     return this.http.post<any>(url, data)
   }
