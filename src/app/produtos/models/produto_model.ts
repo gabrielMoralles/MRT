59ad489cb800a870794245ed495b1806c7535e87
gabrielMoralles <gabriel.barbosa@fcamara.com.br>
2020-06-05 17:54:50 -0300

    idProd

--- src/app/produtos/models/produto_model.ts
+++ src/app/produtos/models/produto_model.ts
@@ -1,11 +1,11 @@
 export interface produto {
-    id:number,
-    nome_Produto :string,
-    valor_Custo:number,
-    valor_Venda:number,
-    qtd_Produto:number,
-    descricao_Produto:string,
-    id_Fornecedor:number
+    id_estoque: number,
+    nome_Produto: string,
+    valor_Custo: number,
+    valor_Venda: number,
+    qtd_Produto: number,
+    descricao_Produto: string,
+    id_Fornecedor: number
 
 
 }
